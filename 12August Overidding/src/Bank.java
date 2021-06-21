27807660c9d5b22c1166abdd716906bc78a95af4
MANITA <MANITA@DESKTOP-LMJT05L>
2021-06-21 21:22:51 +0530

    Maaa

---
+++ 12August Overidding/src/Bank.java
@@ -0,0 +1,20 @@
+
+public class Bank {
+
+	public static void main(String[] args) {
+		
+
+	}
+public void SBI()
+{
+	System.out.println("SBI gives 7% interest");
+	}
+public void Axis()
+{
+	System.out.println("SBI gives 8% interest");
+	}
+public void ICICI()
+{
+	System.out.println("SBI gives 9% interest");
+	}
+}
---
+++ 12August Overidding/src/ICICIBank.java
@@ -0,0 +1,15 @@
+
+public class ICICIBank extends Bank{
+
+	public static void main(String[] args) {
+	ICICIBank a=new ICICIBank();
+a.Axis();
+a.SBI();
+a.ICICI();
+
+	}
+public void Axis()
+{
+System.out.println("Axis provided 8% nterest");	
+}
+}
